e9810ee554df206c970ccbb3a9cfc024b8f2b3bf
wwendyc <wendy.changw@gmail.com>
2018-04-03 09:03:44 -0400

    updated dependencies and added heroku port to app.js

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const db = require('./db')
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 // Logging middleware
 app.use(morgan('dev'))
